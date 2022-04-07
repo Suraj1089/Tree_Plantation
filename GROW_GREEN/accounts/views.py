0bd2ade9167885864587a94339c13922a914349f
Suraj Pisal <surajpisal113@gmail.com>
2022-04-07 23:16:29 +0530

    register page created. view at accounts/register

--- GROW_GREEN/accounts/views.py
+++ GROW_GREEN/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def register(request):  #function for signup user
-    return render(request,'includes/login-signup-modal.html')
+    return render(request,'accounts/register.html')
 
 
 def login(request):   #function for sign in user
