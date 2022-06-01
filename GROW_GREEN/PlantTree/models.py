c6f067f0b3505cc38c6f9cc3387999befc1a66e8
Suraj Pisal <surajpisal113@gmail.com>
2022-06-01 16:05:07 +0530

    change in PlanaTree model PlanTree/models.py

--- GROW_GREEN/PlantTree/models.py
+++ GROW_GREEN/PlantTree/models.py
@@ -1,20 +1,30 @@
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import binascii
+from uuid import uuid4
+import os 
+import random
+
+
+def generate_key(num):
+        return binascii.hexlify(os.urandom(20)).decode()
 
 class PlantaTree(models.Model):
-    #class to plant tree    
+
     tree_choices = [
-        ('N', 'Neem'),
-        ('K','Karanja'),
-        ('M','Mango'),
-        ('J','Jamun'),
-        ('B','Banyan'),
-        ('A','Amrood'),
-        ('BB','Bamboo'),
-        ('BE','Ber'),
-        ('NI','Banyan'),
-        
-    ]
+            ('N', 'Neem'),
+            ('K','Karanja'),
+            ('M','Mango'),
+            ('J','Jamun'),
+            ('B','Banyan'),
+            ('A','Amrood'),
+            ('BB','Bamboo'),
+            ('BE','Ber'),
+            ('NI','Banyan'),
+            
+        ]
+    tree_planted_token = generate_key(random.randint(1,100000))
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
@@ -22,11 +32,11 @@ class PlantaTree(models.Model):
     tree_planted_date = models.DateTimeField(auto_now_add=True)
     tree_pictures = models.ImageField(upload_to='static/tree_planted_images/ %Y/ %m/ %d/')
     tree_description = models.TextField(default=tree_name)
-    # tree_planted_rewards = models.BigIntegerField()
+    points = models.PositiveIntegerField(default=0,verbose_name='points')
     
-    #method for incresing rewards of user
-    def modify_tree_planted_rewards(self,tree_planted_rewards):
-        self.tree_planted_rewards = tree_planted_rewards
+    
+    def modify_points(self,new_points):
+        self.points += new_points
         self.save()
         
     
